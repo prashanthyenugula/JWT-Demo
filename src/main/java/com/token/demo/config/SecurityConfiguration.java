7f67903c5dbd98ffa916dd4090caa8fd8e833775
unknown <cogjava3107@iiht.tech>
2022-08-19 13:05:06 +0530

    Controller is not completed

--- src/main/java/com/token/demo/config/SecurityConfiguration.java
+++ src/main/java/com/token/demo/config/SecurityConfiguration.java
@@ -1,7 +1,9 @@
 package com.token.demo.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -20,6 +22,12 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		
 		auth.userDetailsService(userService);
+	}
+	
+	@Bean
+	public AuthenticationManager authenticationManagerBean() throws Exception {
+		return super.authenticationManagerBean();
+		
 	}
 
 }
